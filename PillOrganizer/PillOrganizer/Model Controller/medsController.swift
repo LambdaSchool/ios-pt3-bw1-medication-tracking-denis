d265b65a26eedf193db02a674137418b2114195c
Denis Cedeno <denis.cedeno@gmail.com>
2019-10-16 23:05:35 -0400

    Created basic storyboard structure

---
+++ PillOrganizer/PillOrganizer/Model Controller/medsController.swift
@@ -0,0 +1,9 @@
+//
+//  medsController.swift
+//  PillOrganizer
+//
+//  Created by denis cedeno on 10/16/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import Foundation
---
+++ PillOrganizer/PillOrganizer/Views/dailyMedsViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  dailyMedsViewCell.swift
+//  PillOrganizer
+//
+//  Created by denis cedeno on 10/16/19.
+//  Copyright © 2019 DenCedeno Co. All rights reserved.
+//
+
+import UIKit
+
+class dailyMedsViewCell: UITableViewCell {
+
+    @IBOutlet weak var weekdayLabel: UILabel!
+    
+    @IBOutlet weak var numberOfMeds: UILabel!
+}
